f79ef07af3f2814d15773c8996985c79e9951f8d
Onkar Gupta <onkargupta0864@gmail.com>
2024-07-24 20:37:19 +0530

    added api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import Navbar from './navbar'
 import Story from './story'
 import data from './data.jsx'
+import { useEffect , useState} from 'react'
 
 function App() {
-  const story = data.map((item) => {
+  const story = data.map((posts) => {
     return (
       <Story
-        {...item}
+        {...posts}
       />
     )
   })
-    
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json()).then(e => setPosts(e))
+  }, [])
   return (
     <div className='flex-col'>
       < Navbar/>
       {story}
+      {
+        posts.map((post) => {
+          return(
+            <li key={post.id}>{post.title}</li> 
+          )
+        })
+      }
     </div>
   )
 }
